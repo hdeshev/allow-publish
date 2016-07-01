8bcb38d197ac567e7d3edec8233ef548a440f01b
Hristo Deshev <hristo@deshev.com>
2016-07-01 11:26:37 +0300

    Log details when allowing publishing

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ var packageConfigs = process.argv.filter(function(arg) {
 console.log("allow-publish: " + packageConfigs);
 
 packageConfigs.forEach(function(packageConfigPath) {
+    console.log("Allowing publishing for: " + packageConfigPath);
     var contents = fs.readFileSync(packageConfigPath, "utf8");
-    var config = JSON.parse(contents);
-    config.private = false;
 
+    var config = JSON.parse(contents);
+    console.log("Package: " + config.name);
+
+    config.private = false;
     fs.writeFileSync(packageConfigPath, JSON.stringify(config, null, "    "));
 });
